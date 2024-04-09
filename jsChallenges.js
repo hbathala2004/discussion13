d7c8de774140863beeab8087a80b718a8c2f985f
hbathala2004 <156857050+hbathala2004@users.noreply.github.com>
2024-04-09 17:53:24 -0400

    Add files via upload

---
+++ jsChallenges.js
@@ -0,0 +1,15 @@
+document.querySelector('#subscribe').addEventListener('click',function)()
+
+console.log("subscribed shows up")
+if(this.checked) {
+  document.querySelector(#emailDiv).style 
+  display="block";
+
+}
+else{
+     document.querySelector(#emailDiv).style/
+     display="block";
+}
+else{
+    document.querySelector(#email.Div).style.display="none"
+    
